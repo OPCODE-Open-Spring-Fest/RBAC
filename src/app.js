95bb696bd57f6a0e1af627d01bb6536463525bea
satyam-code45 <satyam45.dev@gmail.com>
2025-10-15 15:06:23 +0530

    feat: add simple rate limiter

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import cookieparser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
+import rateLimiter from './middlewares/rateLimiter.js';
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_URL,
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieparser());
+
+app.use(rateLimiter);
 
 // Routes
 app.use('/api/auth', authRoutes);
