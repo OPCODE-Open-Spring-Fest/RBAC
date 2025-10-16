6e24befe3a6910b9ae8cecc18e75db906a993733
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2025-10-16 21:08:17 +0530

    feat: add Joi validation schemas for authentication and RBAC routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { registerUser,loginUser } from '../controllers/authController.js';
+import { registerSchema, loginSchema } from '../validations/authValidation.js';
+import {validateMiddleware} from '../middlewares/validate.middleware.js';
+
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', validateMiddleware(registerSchema), registerUser);
+router.post('/login', validateMiddleware(loginSchema), loginUser);
 
 export default router;
