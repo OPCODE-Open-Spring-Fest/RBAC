8322f4d1c4c490d7820d7fdfe3b6b67bf4f296d1
nitingupta95 <ng61315@gmail.com>
2025-10-17 14:43:18 +0530

    Implement Logging and Security Headers

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import cors from 'cors';
 import cookieparser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
 import rbacRoutes from './routes/rbacRoutes.js';
-
+import helmet from 'helmet';
+import morgan from 'morgan';
+import logger from './config/logger.js';
 const app = express();
-
+app.use(helmet());
 app.use(cors({
-  origin: process.env.CORS_URL,
+  origin: '*',
   credentials: true
 }));
 
@@ -15,7 +17,15 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieparser());
+app.use(morgan("combined", { stream: logger.stream }));
 
+app.use(
+  morgan("tiny", {
+    stream: {
+      write: (message) => logger.http(message.trim()),
+    },
+  })
+);
 //routes
 app.use('/api/auth', authRoutes);
 app.use('/api/rbac-test', rbacRoutes);
